1ad228e0479515c9c6b4c45ec2759552b8d06187
omarhuba <omarhuba@gmail.com>
2022-02-11 15:12:56 +0100

    fixt login page

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
 import About from "../views/About.vue";
 import Poster from "../components/Poster.vue";
+import Login from "../components/Login.vue";
 
 Vue.use(VueRouter);
 
@@ -21,6 +22,11 @@ const routes = [
         path: "/Home/poster/:id",
         name: "Poster",
         component: Poster,
+    },
+    {
+        path: "/Login",
+        name: "Login",
+        component: Login,
     },
 ];
 
